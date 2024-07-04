736010f7ae07f0c66585b70e21168c847e6d6df4
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-07-04 23:20:40 +0800

    Feat: Decoded secretkey

--- demo/src/app/api/web3/create/route.ts
+++ demo/src/app/api/web3/create/route.ts
@@ -1,4 +1,5 @@
 import { Keypair } from "@solana/web3.js";
+import bs58 from "bs58";
 import * as fs from "fs";
 
 export async function POST() {
@@ -6,11 +7,13 @@ export async function POST() {
 
   let secretKeyPath = "src/app/api/wallet/wallet.json";
   fs.writeFileSync(secretKeyPath, "[" + kp.secretKey.toString() + "]");
+  const privkey = bs58.encode(new Uint8Array(kp.secretKey));
 
   return new Response(
     JSON.stringify({
       publicKey: kp.publicKey.toBase58(),
       secretKey: kp.secretKey,
+      secretAddress: privkey,
     }),
     {
       headers: {
